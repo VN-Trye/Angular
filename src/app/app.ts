4aed79bec4cab7f935b3d036f8796041ac0d7ea9
VN-Trye <phamlehoangphuongtrye@gmail.com>
2026-05-04 17:04:23 +0700

    CODE 1-9

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayout } from './shared/header-layout/header-layout';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayout, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-new');
+  //text
+  title = {
+    name: 'Phuong',
+    old: 2004,
+  };
+
+  //properties
+  isDisabled = false;
+  handleClick() {
+    console.log('Da bam nut');
+    alert('Ban vua bam nut');
+  }
+
+  //attribute
+  contentImage = 'Trye Wellcome';
+
+  //event
+  nameBtn = ('Click!');
+  clickMessage= '';
+  handleClickMe(): void {
+    this.clickMessage = 'xin chao ban!';
+}
+
+updateField(): void {
+  console.log('Da cap nhat truong');
+}
+
+//two way data binding
+bindingMessage = '';
 }
